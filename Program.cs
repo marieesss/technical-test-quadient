6a9af2a94aeaf3f240a7c5ab4ea08b9322bf5423
Marie <mje.espinosa@gmail.com>
2026-05-10 22:39:17 +0200

    dynamic arguments

--- Program.cs
+++ Program.cs
@@ -2,20 +2,37 @@
 
 ISuggestionService suggestionService = new SuggestionService();
 
-List<string> choices =
-[
-    "gros",
-    "gras",
-    "graisse",
-    "go",
-    "ros",
-    "gro"
-];
+
+// Validate arguments
+if (args.Length < 3)
+{
+    Console.WriteLine("How to run:");
+    Console.WriteLine("dotnet run <term> <numberOfSuggestions> <choice1> <choice2> <choice3> ...");
+    Console.WriteLine();
+    Console.WriteLine("Example:");
+    Console.WriteLine("dotnet run gros 2 gros gras graisse go ros gro");
+    return;
+}
+
+// Get the term to compare
+string term = args[0];
+
+// Get the number of suggestions to return
+if (!int.TryParse(args[1], out int numberOfSuggestions))
+{
+    Console.WriteLine("The number of suggestions must be a valid number.");
+    return;
+}
+
+// Retrieve list of choices
+List<string> choices = args
+    .Skip(2)
+    .ToList();
 
 IEnumerable<string> suggestions = suggestionService.GetSuggestions(
-    "gros",
+    term,
     choices,
-    2
+    numberOfSuggestions
 );
 
 Console.WriteLine("Suggestions trouvées :");
